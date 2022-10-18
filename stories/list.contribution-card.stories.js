ec0e6ef9234b2923f39fcb5db8f595dc7367baaf
Jose Maria CLi <chema9703@gmail.com>
2022-10-17 21:06:56 -0500

    Add first approach to the contribution card component

---
+++ stories/list.contribution-card.stories.js
@@ -0,0 +1,14 @@
+import { ContributionCard } from "../src/client/ui/components/shared/contribution-card";
+
+export default {
+  title: "List/ContributionCard",
+  component: ContributionCard
+}
+
+export const EmptyContributionCard = () => {
+  return <ContributionCard />
+}
+
+EmptyContributionCard.args = {
+
+}
