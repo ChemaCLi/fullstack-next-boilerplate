24dfb06c048bfaea33a4323cd5fc1e7fcadf6ce0
Jose Maria CLi <chema9703@gmail.com>
2022-10-17 23:43:49 -0500

    Add story

--- stories/list.contribution-card.stories.js
+++ stories/list.contribution-card.stories.js
@@ -5,10 +5,19 @@ export default {
   component: ContributionCard
 }
 
-export const EmptyContributionCard = () => {
-  return <ContributionCard />
+export const FilledWithData = (args) => {
+  return (
+    <ContributionCard {...args} />
+  )
 }
 
-EmptyContributionCard.args = {
-
+FilledWithData.args = {
+  unoptimized: true,
+  type: "Video",
+  articleName: "How to model 3D characters with ThreeJS and React (GitHub repo available)",
+  imageUrl: "https://upload.wikimedia.org/wikipedia/commons/b/be/X-4_with_Female_Computer_-_GPN-2000-001932.jpg",
+  originalPostUrl: "https://google.com",
+  authorName: "Luis Anaya",
+  publicationDate: "2022-10-17",
+  authorPhotoUrl: "https://upload.wikimedia.org/wikipedia/commons/b/be/X-4_with_Female_Computer_-_GPN-2000-001932.jpg"
 }
