3fd3830d0e399801b79c56dc6f39303906888d9e
Jose Maria CLi <chema9703@gmail.com>
2022-10-17 22:45:10 -0500

    Move styles to the CSS file

--- src/client/ui/components/shared/contribution-card/index.js
+++ src/client/ui/components/shared/contribution-card/index.js
@@ -2,46 +2,43 @@ import { Card } from "../card";
 import Image from "next/image";
 import { ActionButtons } from "../buttons/action-buttons";
 import { Avatar } from "../avatar";
+import styles from "./ContributionCard.module.css"
 
 export const ContributionCard = () => {
   return (
-    <Card style={{ padding: 0, width: "300px", height: "350px" }}>
-      <Image
-        unoptimized
-        objectFit="cover"
-        width="300px"
-        height="150px"
-        src="https://upload.wikimedia.org/wikipedia/commons/b/be/X-4_with_Female_Computer_-_GPN-2000-001932.jpg" />
-      <div
-        style={{ padding: "8px" }}
-        className="InfoContainer">
-        <div
-          className="AuthorBadge"
-          style={{
-            display: "flex",
-            flexDirection: "row"
-          }}>
-          <Avatar
-            unoptimized
-            size="small"
-            imageUrl="https://upload.wikimedia.org/wikipedia/commons/b/be/X-4_with_Female_Computer_-_GPN-2000-001932.jpg" />
-          <div style={{ display: "block", marginLeft: "8px" }}>
-            <strong>Luis Anaya</strong>
-            <p>17 Oct 2022</p>
+    <div className={styles.ContributionCard}>
+      <Card
+        style={{ padding: 0, width: "350px", height: "320px" }}>
+        <Image
+          unoptimized
+          objectFit="cover"
+          width="350px"
+          height="150px"
+          src="https://upload.wikimedia.org/wikipedia/commons/b/be/X-4_with_Female_Computer_-_GPN-2000-001932.jpg" />
+        <div className={styles.ContributionCard__InfoContainer}>
+          <div className={styles.ContributionCard__AuthorBadge}>
+            <Avatar
+              unoptimized
+              size="small"
+              imageUrl="https://upload.wikimedia.org/wikipedia/commons/b/be/X-4_with_Female_Computer_-_GPN-2000-001932.jpg" />
+            <div className={styles.ContributionCard__PostInfo}>
+              <strong>Luis Anaya</strong>
+              <p style={{ margin: 0 }}>17 Oct 2022</p>
+            </div>
+          </div>
+          <div className={styles.ContributionCard__ArticleHeading}>
+            <a href={"https://google.com"} target="_blank" rel="noopener noreferrer">
+              <h3 className={styles.ContributionCard__ArticleTitle}>
+                How to model 3D characters with ThreeJS and React (GitHub repo available)
+              </h3>
+            </a>
+            <p className={styles.ContributionCard__ArticleType}>Video</p>
+          </div>
+          <div className={styles.ContributionCard__ArticleOptions}>
+            <ActionButtons />
           </div>
         </div>
-        <div className="ArticleHeading" style={{ marginLeft: "48px" }}>
-          <h3>
-            How to model 3D characters with ThreeJS
-          </h3>
-          <p>Video</p>
-        </div>
-        <div
-          className="ArticleOptions"
-          style={{ display: "flex", justifyContent: "flex-end" }}>
-          <ActionButtons />
-        </div>
-      </div>
-    </Card>
+      </Card>
+    </div>
   )
 }
